f2c222dde73f59a70770dd856b6f5476aa3afce1
ibrahimkultepe <ibrahimkultepe@gmail.com>
2023-07-24 12:35:15 +0300

    add sk photo browser

--- Utilities/Utilities/Constant/Closures.swift
+++ Utilities/Utilities/Constant/Closures.swift
@@ -5,7 +5,10 @@
 //  Created by İbrahim Kültepe on 20.04.2023.
 //
 
+import SKPhotoBrowser
+
 public typealias VoidClosure = (() -> ())
 public typealias StringClosure = ((String) -> ())
 public typealias IntClosure = ((Int) -> ())
 public typealias IndexPathClosure = ((IndexPath) -> ())
+public typealias SKPhotoBrowserClosure = ((SKPhotoBrowser) -> ())
